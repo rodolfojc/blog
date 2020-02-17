48be44a1703d44b239d3f9880199796d8cf7715b
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-17 22:58:35 +0000

    Wrapping the Navigator

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 
@@ -12,4 +13,8 @@ const navigator = createStackNavigator ({
   }
 });
 
-export default createAppContainer(navigator);
+const App = createAppContainer(navigator);
+
+export default () => {
+  return <App />;
+};
