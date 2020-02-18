e1c62f9f53f45c52bf3c547a7cf5faa537ef1e8e
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-18 19:19:40 +0000

    Updating with UseReduce

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -1,16 +1,23 @@
-import React, { useState } from 'react';
+import React, { useReducer } from 'react';
 
 const BlogContext = React.createContext();
 
+const blogReducer = (state, action) => {
+
+    switch(action.type){
+        case 'add_blopost':
+            return [...state, {title: `Blog Post #${state.length + 1}`}]
+        default:
+            return state;
+    }
+};
+
 export const BlogProvider = ({ children }) => {
     
-    const [blogPosts, setBlogPosts] = useState([]);
+    const [blogPosts, dispath] = useReducer(blogReducer, []);
 
     const addBlogPost = () => {
-        setBlogPosts([
-            ... blogPosts, 
-            {title: `Blog Post #${blogPosts.length + 1}`}
-        ]);
+        dispath({type: 'add_blopost'});
     };
 
     return (
