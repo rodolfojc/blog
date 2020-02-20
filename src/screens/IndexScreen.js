c760e157f916e63b918b5fb936e63f12248f60e8
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-20 18:52:17 +0000

    A Bit of Styling

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { View, Text, StyleSheet, FlatList, Button } from 'react-native';
 
 import { Context } from '../context/BlogContext';
+import { Feather } from '@expo/vector-icons';
 
 const IndexScreen = () => {
 
@@ -9,7 +10,6 @@ const IndexScreen = () => {
 
     return(
         <View>
-            <Text>Index Page</Text>
             <Button 
                 title="Add Post"
                 onPress={addBlogPost} 
@@ -18,13 +18,35 @@ const IndexScreen = () => {
                 data={state}
                 keyExtractor={blogPost => blogPost.title}
                 renderItem={({item}) => {
-                    return <Text>{item.title}</Text>
+                    return (
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <Feather style={styles.icon} name="trash"/>
+                        </View>
+                    );
                 }}
             />
+            
         </View>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 20, 
+        borderTopWidth: 1,
+        borderColor: 'grey',
+
+    },
+    title: {
+        fontSize: 18,
+    },
+    icon: {
+        fontSize: 24
+    }
+});
 
 export default IndexScreen;
