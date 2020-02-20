05ed074f59c0bcd9bcc3a95dff6a89e0367522cc
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-20 19:15:49 +0000

    Navigation on Tap

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -4,7 +4,7 @@ import { View, Text, StyleSheet, FlatList, Button, TouchableOpacity } from 'reac
 import { Context } from '../context/BlogContext';
 import { Feather } from '@expo/vector-icons';
 
-const IndexScreen = () => {
+const IndexScreen = ({ navigation }) => {
 
     const {state, addBlogPost, deleteBlogPost} = useContext(Context);
 
@@ -19,12 +19,14 @@ const IndexScreen = () => {
                 keyExtractor={blogPost => blogPost.title}
                 renderItem={({item}) => {
                     return (
-                        <View style={styles.row}>
-                            <Text style={styles.title}>{item.title}</Text>
-                            <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
-                                <Feather style={styles.icon} name="trash"/>
-                            </TouchableOpacity>
-                        </View>
+                        <TouchableOpacity onPress={() => navigation.navigate('Show', { id: item.id })}>
+                            <View style={styles.row}>
+                                <Text style={styles.title}>{item.title}</Text>
+                                <TouchableOpacity onPress={() => deleteBlogPost(item.id)}>
+                                    <Feather style={styles.icon} name="trash"/>
+                                </TouchableOpacity>
+                            </View>
+                        </TouchableOpacity>
                     );
                 }}
             />
