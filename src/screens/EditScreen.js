b0b75676d25ffd0c3a4965adc0f74139e7ca1258
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-21 20:14:52 +0000

    Initial Form Values

--- src/screens/EditScreen.js
+++ src/screens/EditScreen.js
@@ -11,7 +11,14 @@ const EditScreen = ({ navigation }) => {
         blogPost => blogPost.id === navigation.getParam('id')
     );
 
-    return <BlogPostForm />
+    return (
+        <BlogPostForm
+            initialValues={{ title: blogPost.title, content: blogPost.content }} 
+            onSubmit={(title, content) => {
+                console.log(edit);
+            }}
+        />
+    );
 };
 
 const styles = StyleSheet.create({});
