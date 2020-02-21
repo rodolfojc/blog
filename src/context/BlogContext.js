e3ce694ff7135756fdacfe6c9eb4843c549a1899
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2020-02-21 22:34:56 +0000

    Navigating Backwards

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -26,8 +26,13 @@ const blogReducer = (state, action) => {
 
 const addBlogPost = dispatch => {
     return (title, content, callback) => {
-        dispatch({type: 'add_blogpost', payload: {title, content } });
-        callback();
+        dispatch({
+            type: 'add_blogpost', 
+            payload: {title, content } 
+        });
+        if (callback) {
+            callback();
+        }        
     };
 };
 
@@ -38,11 +43,14 @@ const deleteBlogPost = dispatch => {
 };
 
 const editBlogPost = dispatch => {
-    return (id, title, content) => {
+    return (id, title, content, callback) => {
         dispatch({
             type: 'edit_blogpost', 
             payload: {id, title, content}
         });
+        if (callback) {
+            callback();
+        }  
     };
 };
 
